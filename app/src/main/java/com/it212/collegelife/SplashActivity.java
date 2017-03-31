920d48b77b5894f4cac7d20909540ddbf7aad657
pjhTest <pjh2018008@yahoo.com>
2017-04-01 02:35:07 +0800

    Initial commit

---
+++ app/src/main/java/com/it212/collegelife/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.it212.collegelife;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
